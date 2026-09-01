ed6162108ac400384808e0d27e54db017bca8dac
Николь Журбенко <kindarufy@yandex.ru>
2026-09-01 20:08:56 +0300

    build: remove CRA web vitals bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,10 +11,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
